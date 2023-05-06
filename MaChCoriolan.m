0e6c90233aad98a7467de113218e6421b292fb4f
jgambale <108366736+jgambale@users.noreply.github.com>
2023-05-05 20:00:59 -0400

    Update MaChCoriolan.m

--- MaChCoriolan.m
+++ MaChCoriolan.m
@@ -12,9 +12,10 @@ for j=1:L-k+1
     A(j,:) = X(j:j+k-1);
 end
 [C,IA,IC] = unique(A,'rows');
-eos = IC(end);
+eos = IC(end); % IC contains the transitions throughout the states.
 % Find all states with multiple unique transitions. These are the only
-% states that need to be arithmetically encoded.
+% states that would need to be arithmetically encoded. A state with only
+% one transition needs no arithmetic code.
 mon = [];
 for i=1:numel(IA)
     icf = find(IC==i);
@@ -28,22 +29,22 @@ end
 
 % Our emissions are the end letter of the block.
 [HC, HIA, HIC] = unique(X); % All of these are possible outputs of our Markov Chain.
-N1 = size(C,1); S1 = sparse(N1,N1)+1;E1 = sparse(numel(IA),numel(HC))+1; % Markov States, Making the Transition Matrix.
-emlist = HIC(2:numel(HIC)-1);
+N1 = size(C,1); S1 = sparse(N1,N1)+1;E1 = sparse(numel(IA),numel(HC))+1; % Markov States, Making the Transition Matrix. All transitions/emissions get one weight.
+emlist = HIC(2:numel(HIC)-1); % Emission List.
 for i=1:L-k
     S1(IC(i),IC(i+1)) = S1(IC(i),IC(i+1)) + 100; % Large weight to transitions that are present.
     linu = emlist(i);
-    E1(IC(i),linu) = E1(IC(i),linu) + 100;
+    E1(IC(i),linu) = E1(IC(i),linu) + 100; % Large weight to emissions that are present.
 end
 
 esttr = S1./(eps+sum(S1,2)); % Estimate of the transition probabilities.
-estemit = E1./sum(E1,2);
+estemit = E1./sum(E1,2); % Estimate of the emission probabilities.
 
 y = {}; % States we hit.
-sec = zeros(1,numel(mon)); % State Entropy Matrix.
+sec = zeros(1,numel(mon)); % State Entropy vector (for each multi-transmission state)
 for i=1:numel(mon)
     siq = mon(i); % State to encode.
-    icf = find(IC==siq);
+    icf = find(IC==siq); 
     if siq==eos
         [Cs, IAs, ICs] =  unique(IC(icf(1:numel(icf)-1)+1));
     else
@@ -56,7 +57,7 @@ for i=1:numel(mon)
     code{i} = arithenco(ICs,sq); % Code for state list.
 end
 
-% Finding the stationart probability of states.
+% Finding the stationary probability of states.
 [V,D] = eigs(esttr',6);
 D=diag(D);
 Da = abs(D);
@@ -86,7 +87,7 @@ end
 % The emission probabilities imply that each state emits is strongly
 % predicted to output one output only. So only encode the state transitions.
 for i=1:numel(mon)
-    siq = mon(i);sq = ceil(10000*trref(siq,:))+1;sq2 = trref(siq,:)+0.0001;sq2 = sq2/sum(sq2);
+    siq = mon(i);sq = ceil(10000*trref(siq,:))+1;sq2 = trref(siq,:)+0.0001;sq2 = sq2/sum(sq2); % Give every output a possibility, in case the hmmtrain broke.
     icf = find(IC==siq);
     if siq==eos
         Cs = IC(icf(1:numel(icf)-1)+1);
@@ -116,4 +117,3 @@ for i=1:numel(code)
 end
 % Arithmetical Code Bits Per Symbol
 acb2 = cls2/numel(IC);
-% Weirdly this is smaller than the theoretical, but the theoretical is not perfectly calculated.
